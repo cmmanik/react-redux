a81b3fb036ed9d3cf1dc86d49ac377cd994cd120
cmmanik <mkmanik6523u@gmial.com>
2019-03-20 21:30:00 +0600

    install resux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {createStore} from 'redux';
 import './App.css';
+
+
+
 
 class App extends Component {
   render() {
+
+    const reducer = (state=0, action) =>{
+      switch (action.type) {
+        case 'PLUS':
+          return state+100;
+        case 'MINUS':
+          return state-100
+
+        default:
+          return state;
+      }
+    }
+
+    const store = createStore(reducer);
+    store.subscribe(()=>{
+      console.log(store.getState());
+    })
+
+    store.dispatch({type:'PLUS'})
+    store.dispatch({type:'PLUS'})
+    store.dispatch({ type:'MINUS'})
+    store.dispatch({ type:'MINUS'})
+    store.dispatch({ type:'PLUS'})
+
+
     return (
       <div className="App">
         <header className="App-header">
